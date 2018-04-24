7ecc6777b174e26f0ccf1c2f05f6956528836fb5
zhouyicang <2851520703@qq.com>
2018-04-24 14:04:36 +0800

    提交项目结构

---
+++ footprint-demo/src/main/java/org/footprint/demo/controller/SampleController.java
@@ -0,0 +1,12 @@
+package org.footprint.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
