d56619e9b93582d3fdef94bded9f2fec0f7fff77
zhouyicang <2851520703@qq.com>
2018-04-25 17:25:19 +0800

    公司提交代码

--- footprint-demo/src/main/java/org/footprint/demo/App.java
+++ footprint-demo/src/main/java/org/footprint/demo/App.java
@@ -2,12 +2,14 @@ package org.footprint.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ComponentScan(basePackages="org.footprint")
 public class App 
 {
     public static void main( String[] args )
